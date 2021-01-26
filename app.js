78f5c10b20a132d58ee3f3e03e2e63b83a1a1f89
Kireck211 <erickdesantiago11@gmail.com>
2021-01-26 08:32:15 -0600

    feat: console logged two hi

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+console.log('hi');
+console.log('hi');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
