b666962c2e9b09ca0507f747abbee4bee719d2f0
Kireck211 <erickdesantiago11@gmail.com>
2021-01-29 08:37:21 -0600

    feat: added mongoose on app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const mongoose = require('mongoose');
 
 
 var indexRouter = require('./routes/index');
